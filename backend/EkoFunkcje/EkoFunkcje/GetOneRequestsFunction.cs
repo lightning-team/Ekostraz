051ef8fbe0e6d0684e57316dde3269d2c14f1f0d
Karol Przybylak <karolprzybylak@outlook.com>
2019-06-29 18:51:10 +0200

    Added get one Request Function

---
+++ backend/EkoFunkcje/EkoFunkcje/GetOneRequestsFunction.cs
@@ -0,0 +1,55 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EkoFunkcje
+{
+    public static class GetOneRequestsFunction
+    {
+        [FunctionName("GetOneRequestsFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] IdReqest reqest,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<InterventionEntity, InterventionItemDto>()
+                .ForMember(dest => dest.Id,
+                    opts => opts.MapFrom(src => src.PartitionKey)));
+            var mapper = config.CreateMapper();
+
+            var storageAccountConnectionString = Environment.GetEnvironmentVariable("StorageAccountConnectionString", EnvironmentVariableTarget.Process);
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable interventionTable = tableClient.GetTableReference("Intervention");
+
+
+            TableContinuationToken token = null;
+            var entities = new List<InterventionItemDto>();
+            do
+            {
+                var queryResult = await interventionTable.ExecuteQuerySegmentedAsync(new TableQuery<InterventionEntity>().Where(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, reqest.Id)), token);
+                entities.AddRange(queryResult.Results.Select(x => mapper.Map<InterventionItemDto>(x)));
+                token = queryResult.ContinuationToken;
+            } while (token != null);
+
+            return new JsonResult(entities);
+        }
+
+        public class IdReqest
+        {
+            public string Id { get; set; }
+        }
+    }
+}
---
+++ backend/EkoFunkcje/EkoFunkcje/Models/Dto/InterventionItemDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EkoFunkcje
+{
+    public class InterventionItemDto
+    {
+        public string Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string FullName { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Status { get; set; }
+
+        public double GeoLat { get; set; }
+
+        public double GeoLng { get; set; }
+    }
+}
