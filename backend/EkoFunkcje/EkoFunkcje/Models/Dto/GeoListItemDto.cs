ab21ba1f90c225fa158c11ebddf077bf4193629c
Karol Przybylak <karolprzybylak@outlook.com>
2019-06-29 18:25:07 +0200

    Added GetAllGeoDataFunction

---
+++ backend/EkoFunkcje/EkoFunkcje/Models/Dto/GeoListItemDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EkoFunkcje
+{
+    public class GeoListItemDto
+    {
+        public string InterventionId { get; set; }
+
+        public double GeoLat { get; set; }
+
+        public double GeoLng { get; set; }
+    }
+}
