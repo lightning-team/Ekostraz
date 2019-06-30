a0ad475e9d7dcb7ad665c2d6323875460db7c911
Patryk Lotzwi <p.lotzwi@outlook.com>
2019-06-30 09:13:53 +0200

    Added missing description

--- backend/EkoFunkcje/EkoFunkcje/Models/Respones/InterventionListItemResponse.cs
+++ backend/EkoFunkcje/EkoFunkcje/Models/Respones/InterventionListItemResponse.cs
@@ -10,6 +10,8 @@ namespace EkoFunkcje
 
         public string FullName { get; set; }
 
+        public string Description { get; set; }
+
         public int PhoneNumber { get; set; }
 
         public string Email { get; set; }
