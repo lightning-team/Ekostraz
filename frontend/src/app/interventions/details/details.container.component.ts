74180f1e8ed2cc96aaaebef7f2f676d157e15c6d
CorneeSean <radkuc@wp.pl>
2019-06-30 10:38:16 +0200

    Fix details.contianer - load intervention by route params

--- frontend/src/app/interventions/details/details.container.component.ts
+++ frontend/src/app/interventions/details/details.container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ClientIntervention } from '../intervention';
+import { InterventionsService } from '../interventions.service';
 
 @Component({
     selector: 'app-intervention-details-container',
@@ -12,11 +13,25 @@ export class InterventionDetailsContainerComponent implements OnInit {
 
     private intervention: ClientIntervention;
 
-    constructor(private router: Router) {
+    constructor(private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private interventionsService: InterventionsService,
+    ) {
         this.intervention = this.router.getCurrentNavigation().extras.state as ClientIntervention;
     }
 
     ngOnInit() {
+        if (this.intervention) return;
+        this.fetchIntervention();
+    }
 
+    private fetchIntervention() {
+        this.activatedRoute.params.subscribe(params => {
+            if (!params.interventionId) return;
+            this.interventionsService.getIntervention(params.interventionId as string)
+                .subscribe(intervention => {
+                    this.intervention = intervention;
+                });
+        });
     }
 }
