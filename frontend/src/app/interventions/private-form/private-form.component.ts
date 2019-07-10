ba4e41c86e33f2fb619374895c37d88c7091c878
CorneeSean <radkuc@wp.pl>
2019-07-10 20:08:26 +0200

    Make PrivateFormComponent leaner

--- frontend/src/app/interventions/private-form/private-form.component.ts
+++ frontend/src/app/interventions/private-form/private-form.component.ts
@@ -1,32 +1,19 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnDestroy} from '@angular/core';
 
-import { Observable, Subscription } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
-import {ClientIntervention, InterventionFormSubmitData} from '../types';
-import {InterventionsService} from '../interventions.service';
+import { InterventionFormSubmitData } from '../types';
+import { InterventionsService } from '../interventions.service';
 
 @Component({
   selector: 'app-private-intervention-form',
   templateUrl: './private-form.component.html',
   styleUrls: ['./private-form.component.scss']
 })
-export class PrivateFormComponent implements OnInit, OnDestroy {
+export class PrivateFormComponent implements OnDestroy {
   private postSubscription: Subscription | null = null;
 
-  intervention$: Observable<ClientIntervention>;
-
-  constructor(
-      private activatedRoute: ActivatedRoute,
-      private interventionsService: InterventionsService,
-  ) {}
-
-  ngOnInit() {
-    this.intervention$ = this.interventionsService.getIntervention(this.activatedRoute.params).pipe(
-        take(1)
-    );
-  }
+  constructor(private interventionsService: InterventionsService) {}
 
   ngOnDestroy() {
     if (this.postSubscription) {
