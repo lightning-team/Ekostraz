886b85e8be8a11de2dcf2dc717181e23f1fc4925
CorneeSean <radkuc@wp.pl>
2019-07-11 16:11:58 +0200

    Add EditableFormContainer base class. Use it in PrivateEditForm.

--- frontend/src/app/interventions/private-edit-form/private-edit-form.component.ts
+++ frontend/src/app/interventions/private-edit-form/private-edit-form.component.ts
@@ -1,55 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {EditableFormContainer} from '@shared/base';
 
-import {BehaviorSubject} from 'rxjs';
-import {finalize, switchMapTo, take, tap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 import {Intervention, InterventionFormSubmitData} from '../types';
 import {InterventionsService} from '../interventions.service';
-import {ComponentWithSubscriptions} from '@shared/base';
+
 
 @Component({
   selector: 'app-private-edit-intervention-form',
   templateUrl: './private-edit-form.component.html',
   styleUrls: ['./private-edit-form.component.scss']
 })
-export class PrivateEditFormComponent extends ComponentWithSubscriptions implements OnInit {
+export class PrivateEditFormComponent extends EditableFormContainer<InterventionFormSubmitData, Intervention> {
   intervention: Intervention | null = null;
-  loading$: BehaviorSubject<boolean | null> = new BehaviorSubject(true);
 
-  constructor(
-      private activatedRoute: ActivatedRoute,
-      private interventionsService: InterventionsService,
-  ) {
-    super();
+  constructor(private activatedRoute: ActivatedRoute, private interventionsService: InterventionsService) {
+    super(interventionsService.postPrivateForm.bind(interventionsService));
   }
 
-  ngOnInit() {
-    const intervention$ = this.interventionsService.getIntervention(this.activatedRoute.params).pipe(
+  protected getInitialData$(): Observable<Intervention> {
+    return this.interventionsService.getIntervention(this.activatedRoute.params).pipe(
         tap(intervention => this.setIntervention(intervention)),
     );
-
-    const loadDataSubscription = this.loading$.pipe(
-        take(1),
-        switchMapTo(intervention$),
-        finalize(() => this.finishLoading()),
-    ).subscribe();
-
-    this.subscriptions.add(loadDataSubscription);
   }
 
   private setIntervention(intervention: Intervention) {
     this.intervention = intervention;
-  }
-
-  private finishLoading() {
-    this.loading$.next(false);
-    this.loading$.complete();
-  }
-
-  onSubmit(eventData: InterventionFormSubmitData) {
-    this.subscriptions.add(
-        this.interventionsService.postPrivateForm(eventData)
-    );
   }
 }
--- frontend/src/app/shared/base/FormContainer.ts
+++ frontend/src/app/shared/base/FormContainer.ts
@@ -1,12 +1,24 @@
-import {ComponentWithSubscriptions} from './ComponentWithSubscriptions';
 import {Subscription} from 'rxjs';
+import {ComponentWithSubscriptions} from './ComponentWithSubscriptions';
+import {LoadingComponent} from './LoadingComponent';
 
-export declare interface SubmittableForm<T> {
-    onSubmit(eventData: T);
+export declare interface SubmittableForm<SubmitDataType> {
+    onSubmit(eventData: SubmitDataType);
 }
 
-export abstract class FormContainer<T> extends ComponentWithSubscriptions implements SubmittableForm<T> {
-    protected constructor(private submitFormFunction: (T) => Subscription) {
+export abstract class FormContainer<SubmitDataType> extends ComponentWithSubscriptions implements SubmittableForm<SubmitDataType> {
+    protected constructor(private submitFormFunction: (eventData: SubmitDataType) => Subscription) {
+        super();
+    }
+
+    onSubmit(eventData) {
+        this.subscriptions.add(this.submitFormFunction(eventData));
+    }
+}
+
+export abstract class EditableFormContainer<SubmitDataType, InitialData> extends LoadingComponent<InitialData>
+                                                                         implements SubmittableForm<SubmitDataType> {
+    protected constructor(private submitFormFunction: (eventData: SubmitDataType) => Subscription) {
         super();
     }
 
