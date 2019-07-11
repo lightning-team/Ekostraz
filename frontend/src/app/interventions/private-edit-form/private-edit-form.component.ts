6d537b41c986b28fa69089e8ad799c2fe8c5ec80
CorneeSean <radkuc@wp.pl>
2019-07-11 17:19:36 +0200

    Refactor PrivateEditForm to match with LoadingComponent base

--- frontend/src/app/interventions/private-edit-form/private-edit-form.component.ts
+++ frontend/src/app/interventions/private-edit-form/private-edit-form.component.ts
@@ -3,7 +3,6 @@ import {ActivatedRoute} from '@angular/router';
 import {EditableFormContainer} from '@shared/base';
 
 import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
 
 import {Intervention, InterventionFormSubmitData} from '../types';
 import {InterventionsService} from '../interventions.service';
@@ -15,19 +14,11 @@ import {InterventionsService} from '../interventions.service';
   styleUrls: ['./private-edit-form.component.scss']
 })
 export class PrivateEditFormComponent extends EditableFormContainer<InterventionFormSubmitData, Intervention> {
-  intervention: Intervention | null = null;
-
   constructor(private activatedRoute: ActivatedRoute, private interventionsService: InterventionsService) {
     super(interventionsService.postPrivateForm.bind(interventionsService));
   }
 
   protected getInitialData$(): Observable<Intervention> {
-    return this.interventionsService.getIntervention(this.activatedRoute.params).pipe(
-        tap(intervention => this.setIntervention(intervention)),
-    );
-  }
-
-  private setIntervention(intervention: Intervention) {
-    this.intervention = intervention;
+    return this.interventionsService.getIntervention(this.activatedRoute.params);
   }
 }
