bf85011fad96e87bdfdb62fa94bb9c9a942e70d2
CorneeSean <radkuc@wp.pl>
2019-07-11 17:18:42 +0200

    Extend LoadingComponent base class

--- frontend/src/app/shared/base/LoadingComponent.ts
+++ frontend/src/app/shared/base/LoadingComponent.ts
@@ -1,22 +1,33 @@
 import {OnInit} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {finalize, switchMapTo, take} from 'rxjs/operators';
+import {finalize, switchMapTo, take, tap, map} from 'rxjs/operators';
 import {ComponentWithSubscriptions} from './ComponentWithSubscriptions';
 
-export abstract class LoadingComponent<InitialData> extends ComponentWithSubscriptions implements OnInit {
+const identity: (<T>(item: T) => T) = item => item;
+
+export abstract class LoadingComponent<APIData, ViewData = any> extends ComponentWithSubscriptions implements OnInit {
     loading$: BehaviorSubject<boolean | null> = new BehaviorSubject(true);
+    initialData: ViewData|APIData|null = null;
+
+    protected mapInitialData: (data: APIData) => ViewData|APIData = identity;
 
     ngOnInit() {
         const initialData$ = this.getInitialData$();
         const loadDataSubscription = this.loading$.pipe(
             take(1),
             switchMapTo(initialData$),
+            map(data => this.mapInitialData(data)),
+            tap(data => this.setInitialData(data)),
             finalize(() => this.finishLoading()),
         ).subscribe();
         this.subscriptions.add(loadDataSubscription);
     }
 
-    protected abstract getInitialData$(): Observable<InitialData>;
+    protected abstract getInitialData$(): Observable<APIData>;
+
+    private setInitialData(data: ViewData|APIData) {
+        this.initialData = data;
+    }
 
     private finishLoading() {
         this.loading$.next(false);
