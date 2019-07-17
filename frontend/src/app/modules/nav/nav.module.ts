4f5b32f7e398b68430d3ab6f86a45c3fe30d7de0
CorneeSean <radkuc@wp.pl>
2019-07-17 19:00:56 +0200

    Use mat-icons module in Nav

--- frontend/src/app/modules/nav/nav.module.ts
+++ frontend/src/app/modules/nav/nav.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
 import {RouterModule} from '@angular/router';
 
 import {NavComponent} from './nav.component';
@@ -16,6 +16,7 @@ import { MobileNavComponent } from './mobile-nav/mobile-nav.component';
   imports: [
     CommonModule,
     MatButtonModule,
+    MatIconModule,
     MatToolbarModule,
     RouterModule,
   ],
