e5a0d4447b51fdb83cc6621f23c3dd062acb110d
CorneeSean <radkuc@wp.pl>
2019-07-25 00:28:58 +0200

    Minor clean-up in LoginComponent

--- frontend/src/app/modules/auth/login/login.component.ts
+++ frontend/src/app/modules/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, TemplateRef, ViewChild, ViewEncapsulation} from '@angular/core';
-import {MatDialog} from '@angular/material';
+import {MatDialog, MatDialogRef} from '@angular/material';
 import {ComponentWithSubscriptions} from '@shared/base';
 import {AuthService} from '../auth.service';
 
@@ -15,12 +15,22 @@ const DIALOG_PANEL_CLASS = 'login-dialog-panel';
 })
 export class LoginComponent extends ComponentWithSubscriptions implements OnInit {
   @ViewChild('dialogTemplate', {static: true}) dialogTemplate: TemplateRef<any>;
+  dialog: MatDialogRef<any>;
 
   constructor(private dialogService: MatDialog, private authService: AuthService) {
     super();
   }
 
   ngOnInit() {
+    this.dialog = this.createLoginDialog();
+  }
+
+  onLoginClick() {
+    this.authService.logIn();
+    this.dialog.close();
+  }
+
+  private createLoginDialog() {
     const dialog = this.dialogService.open(this.dialogTemplate, {
       disableClose: true,
       backdropClass: DIALOG_BACKDROP_CLASS,
@@ -28,9 +38,7 @@ export class LoginComponent extends ComponentWithSubscriptions implements OnInit
     });
     const dialogSubscription = dialog.afterClosed().subscribe();
     this.subscriptions.add(dialogSubscription);
-  }
 
-  onLoginClick() {
-    this.authService.logIn();
+    return dialog;
   }
 }
