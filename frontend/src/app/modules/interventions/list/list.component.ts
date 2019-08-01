8bb46a03af6dcd5e2a221565643189230c19c051
CorneeSean <radkuc@wp.pl>
2019-08-01 13:51:21 +0200

    Simplify LoadingComponent base

--- frontend/src/app/modules/interventions/list/list.component.ts
+++ frontend/src/app/modules/interventions/list/list.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import {LoadingComponent} from '@shared/base';
 import {Intervention, InterventionListRouterState, ListIntervention} from '../types';
 import {InterventionsService} from '../interventions.service';
-import {Router} from '@angular/router';
-import {LoadingComponent} from '@shared/base';
 
 
 @Component({
@@ -11,21 +14,21 @@ import {LoadingComponent} from '@shared/base';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent extends LoadingComponent<Intervention[], ListIntervention[]> {
-  protected mapInitialData = mapToTableData;
-
+export class ListComponent extends LoadingComponent<ListIntervention[]> {
   constructor(private router: Router, private interventionsService: InterventionsService) {
     super();
   }
 
-  getInitialData$(): Observable<Intervention[]> {
-     return this.interventionsService.fetchInterventions();
+  getInitialData$(): Observable<ListIntervention[]> {
+     return this.interventionsService.fetchInterventions().pipe(
+         map(interventions => mapToTableData(interventions))
+     );
   }
 
-  showMap(interventions: Intervention[]) {
+  showMap() {
     this.router.navigate(
         ['interwencje', 'mapa'],
-        { state: {interventions} } as InterventionListRouterState);
+        { state: {interventions: this.initialData} } as InterventionListRouterState);
   }
 }
 
--- frontend/src/app/shared/base/LoadingComponent.ts
+++ frontend/src/app/shared/base/LoadingComponent.ts
@@ -1,31 +1,27 @@
 import {OnInit} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {finalize, switchMapTo, take, tap, map} from 'rxjs/operators';
+import {finalize, switchMapTo, take, tap} from 'rxjs/operators';
 import {ComponentWithSubscriptions} from './ComponentWithSubscriptions';
 
-const identity = <T>(item: T) => item;
 
-export abstract class LoadingComponent<APIData, ViewData = APIData> extends ComponentWithSubscriptions implements OnInit {
+export abstract class LoadingComponent<ViewData> extends ComponentWithSubscriptions implements OnInit {
     loading$: BehaviorSubject<boolean | null> = new BehaviorSubject(true);
-    initialData: ViewData|APIData|null = null;
-
-    protected mapInitialData: (data: APIData) => ViewData|APIData = identity;
+    initialData: ViewData|null = null;
 
     ngOnInit() {
         const initialData$ = this.getInitialData$();
         const loadDataSubscription = this.loading$.pipe(
             take(1),
             switchMapTo(initialData$),
-            map(data => this.mapInitialData(data)),
             tap(data => this.setInitialData(data)),
             finalize(() => this.finishLoading()),
         ).subscribe();
         this.subscriptions.add(loadDataSubscription);
     }
 
-    protected abstract getInitialData$(): Observable<APIData>;
+    protected abstract getInitialData$(): Observable<ViewData>;
 
-    private setInitialData(data: ViewData|APIData) {
+    private setInitialData(data: ViewData) {
         this.initialData = data;
     }
 
