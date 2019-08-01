6810ce50e46821bdbe6e56447bc17e4b15f1d681
CorneeSean <radkuc@wp.pl>
2019-08-01 13:53:08 +0200

    Create SharedModule with LoaderComponent

---
+++ frontend/src/app/modules/shared/loader/loader.component.ts
@@ -0,0 +1,20 @@
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {Observable} from 'rxjs';
+
+@Component({
+    selector: 'app-loader',
+    template: `
+        <div class="app-spinner-container" *ngIf="loading$ | async; else content">
+            <mat-spinner [diameter]="40"></mat-spinner>
+        </div>
+        <ng-template #content>
+            <ng-content ></ng-content>
+        </ng-template>
+    `,
+    styleUrls: ['./loader.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoaderComponent {
+    @Input() loading$: Observable<boolean>;
+}
+
