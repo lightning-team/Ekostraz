50b9413bde9b8ebbe7dc8c319e7527922ba499b9
CorneeSean <radkuc@wp.pl>
2019-08-03 00:16:18 +0200

    Add ability to delay hiding of loader to LoadingComponent

--- frontend/src/app/shared/base/LoadingComponent.ts
+++ frontend/src/app/shared/base/LoadingComponent.ts
@@ -1,22 +1,35 @@
 import {OnInit} from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, EMPTY, Observable} from 'rxjs';
 import {finalize, switchMapTo, take, tap} from 'rxjs/operators';
 import {ComponentWithSubscriptions} from './ComponentWithSubscriptions';
 
 
 export abstract class LoadingComponent<ViewData> extends ComponentWithSubscriptions implements OnInit {
     loading$: BehaviorSubject<boolean> = new BehaviorSubject(true);
-    initialData: ViewData|null = null;
+    initialData: ViewData | null = null;
+
+    /**
+     * Override with custom observable or add pipe actions, if you want some effects to happen before
+     * the loader is hidden, but after initial data has already been loaded.
+     * Defaults to an empty observable that completes immediately.
+     */
+    protected afterDataLoaded$: Observable<any> = EMPTY;
 
     ngOnInit() {
+        this.startDataLoading();
+    }
+
+    private startDataLoading() {
         const initialData$ = this.getInitialData$();
-        const loadDataSubscription = this.loading$.pipe(
-            take(1),
-            switchMapTo(initialData$),
-            tap(data => this.setInitialData(data)),
-            finalize(() => this.finishLoading()),
-        ).subscribe();
-        this.subscriptions.add(loadDataSubscription);
+        this.subscriptions.add(
+            this.loading$.pipe(
+                take(1),
+                switchMapTo(initialData$),
+                tap(data => this.setInitialData(data)),
+                switchMapTo(this.afterDataLoaded$),
+                finalize(() => this.finishLoading()),
+            ).subscribe()
+        );
     }
 
     protected abstract getInitialData$(): Observable<ViewData>;
