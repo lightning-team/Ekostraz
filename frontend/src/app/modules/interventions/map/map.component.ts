2fe37e3bf965454a49420eddc4c0ad24f9f034f7
CorneeSean <radkuc@wp.pl>
2019-08-03 16:41:36 +0200

    Show Map after all map tiles have loaded

--- frontend/src/app/modules/interventions/map/map.component.ts
+++ frontend/src/app/modules/interventions/map/map.component.ts
@@ -1,9 +1,12 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component, ViewChild, ViewContainerRef} from '@angular/core';
+import {AgmMap, GoogleMapsAPIWrapper} from '@agm/core';
 import {LoadingComponent} from '@shared/base';
+
+import {Observable, of, Subject} from 'rxjs';
+import {switchMapTo, take, tap} from 'rxjs/operators';
 
 import {Intervention} from '../types';
 import {InterventionsService} from '../interventions.service';
-import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-interventions-map',
@@ -12,9 +15,13 @@ import {Subject} from 'rxjs';
 })
 export class MapComponent extends LoadingComponent<Intervention[]> {
   private mapReadySubject = new Subject();
-  afterDataLoaded$ = this.mapReadySubject.asObservable();
+  // TODO: This is a work-around for missing GoogleMaps API 'tilesloaded' event.
+  // Remove when AgmMaps exposes 'tilesloaded' event in its component API.
+  afterDataLoaded$ = this.waitForMapTilesLoaded();
 
-  constructor(private interventionsService: InterventionsService) {
+  @ViewChild(AgmMap, {static: false, read: ViewContainerRef}) agmMap: ViewContainerRef;
+
+  constructor(private interventionsService: InterventionsService, private changeDetector: ChangeDetectorRef) {
     super();
   }
 
@@ -22,11 +29,25 @@ export class MapComponent extends LoadingComponent<Intervention[]> {
     return this.interventionsService.getInterventions();
   }
 
-  onMapIdle() {
-    this.hideLoaderOnFirstIdle();
+  private waitForMapTilesLoaded(): Observable<any> {
+    return of(null).pipe(
+        // ViewChild agmMap will be initialized only after change detection run.
+        tap(() => this.changeDetector.detectChanges()),
+        tap(() => this.attachTilesLoadedListener()),
+        switchMapTo(this.mapReadySubject.asObservable()),
+    );
   }
 
-  private hideLoaderOnFirstIdle() {
+  private attachTilesLoadedListener() {
+    const mapsAPIWrapper = this.agmMap.injector.get(GoogleMapsAPIWrapper);
+    const tilesloaded$ = mapsAPIWrapper.subscribeToMapEvent('tilesloaded');
+    tilesloaded$.pipe(
+        take(1),
+        tap(this.hideLoader.bind(this))
+    ).subscribe();
+  }
+
+  private hideLoader() {
     if (!this.mapReadySubject.isStopped) {
       this.mapReadySubject.complete();
     }
