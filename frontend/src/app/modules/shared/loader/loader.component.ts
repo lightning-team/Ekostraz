264e1f87f736d9e1a2fdc72c898f7e84bd5a1b3f
CorneeSean <radkuc@wp.pl>
2019-08-17 16:15:14 +0200

    refactor: Clean-up in Loader component

--- frontend/src/app/modules/shared/loader/loader.component.ts
+++ frontend/src/app/modules/shared/loader/loader.component.ts
@@ -1,6 +1,7 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 
-import {Observable} from 'rxjs';
+import {combineLatest, Observable, of} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 import {Required} from '@shared/decorators';
 import {fadeInOut} from '../animations';
@@ -17,11 +18,10 @@ import {fadeInOut} from '../animations';
 
         <!-- NOTE: We use ngClass below to make sure that content-wrapper is hidden in CSS
              before Angular animations get applied. This way we don't get any temporary glitches. -->
-        <!-- TODO: Clean up common logic from the template -->
         <div class='content-wrapper'
-             [ngClass]="(loading$ | async) && renderHiddenBeforeLoaded ? 'hidden' : ''"
-             [@fadeInOut]="(loading$ | async) && renderHiddenBeforeLoaded ? 'hidden' : ''"
-             *ngIf="(loading$ | async) === false || renderHiddenBeforeLoaded">
+             [ngClass]="hidden$ | async"
+             [@fadeInOut]="hidden$ | async"
+             *ngIf="renderContent$ | async">
             <ng-content></ng-content>
         </div>
     `,
@@ -29,10 +29,27 @@ import {fadeInOut} from '../animations';
     changeDetection: ChangeDetectionStrategy.OnPush,
     animations: [fadeInOut],
 })
-export class LoaderComponent {
+export class LoaderComponent implements OnInit {
     @Input() @Required() loading$: Observable<boolean>;
     @Input() loaderText = '';
     /** Flag deciding whether the content should be rendered before load finished, but hidden by CSS. */
     @Input() renderHiddenBeforeLoaded = false;
+
+    renderContent$: Observable<boolean>;
+    hidden$: Observable<string>;
+
+    ngOnInit() {
+        this.initDerivedTemplateObservables();
+    }
+
+    private initDerivedTemplateObservables() {
+        const loadingHidden$ = combineLatest(this.loading$, of(this.renderHiddenBeforeLoaded));
+        this.renderContent$ = loadingHidden$.pipe(
+            map(([loading, renderHidden]) => !loading || renderHidden)
+        );
+        this.hidden$ = loadingHidden$.pipe(
+            map(([loading, renderHidden]) => loading && renderHidden ? 'hidden' : '')
+        );
+    }
 }
 
