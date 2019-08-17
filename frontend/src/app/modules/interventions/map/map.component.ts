da81b09da06b208aee5423ed127156016ee2ab82
CorneeSean <radkuc@wp.pl>
2019-08-17 10:27:41 +0200

    refactor(MapComponent): Use AGM native tilesLoaded event

--- frontend/src/app/modules/interventions/map/map.component.ts
+++ frontend/src/app/modules/interventions/map/map.component.ts
@@ -1,9 +1,8 @@
-import {ChangeDetectorRef, Component, ViewChild, ViewContainerRef} from '@angular/core';
-import {AgmMap, GoogleMapsAPIWrapper} from '@agm/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import {LoadingComponent} from '@shared/base';
 
-import {Observable} from 'rxjs';
-import {exhaustMap, take} from 'rxjs/operators';
+import {Subject} from 'rxjs';
+import {take} from 'rxjs/operators';
 
 import {Intervention} from '../types';
 import {InterventionsService} from '../interventions.service';
@@ -14,9 +13,8 @@ import {InterventionsService} from '../interventions.service';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent extends LoadingComponent<Intervention[]> {
-  afterDataLoaded$ = this.waitForMapTilesLoaded();
-
-  @ViewChild(AgmMap, {static: false, read: ViewContainerRef}) agmMap: ViewContainerRef;
+  tilesLoadedSubject = new Subject();
+  afterDataLoaded$ = this.tilesLoadedSubject.asObservable().pipe(take(1));
 
   constructor(private interventionsService: InterventionsService, changeDetector: ChangeDetectorRef) {
     super(changeDetector);
@@ -26,20 +24,7 @@ export class MapComponent extends LoadingComponent<Intervention[]> {
     return this.interventionsService.getInterventions();
   }
 
-  private waitForMapTilesLoaded(): Observable<any> {
-    return this.afterDataLoaded$.pipe(
-        exhaustMap(() => this.attachTilesLoadedListener()),
-        take(1)
-    );
-  }
-
-  /**
-   * TODO: This is a work-around for missing GoogleMaps API 'tilesloaded' event.
-   * Remove when AgmMaps exposes 'tilesloaded' event in its component API.
-   */
-  private attachTilesLoadedListener() {
-    const mapsAPIWrapper = this.agmMap.injector.get(GoogleMapsAPIWrapper);
-    const tilesloaded$ = mapsAPIWrapper.subscribeToMapEvent('tilesloaded');
-    return tilesloaded$.pipe(take(1));
+  onTilesLoaded() {
+    this.tilesLoadedSubject.next();
   }
 }
