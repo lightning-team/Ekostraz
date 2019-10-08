d46f54ad80a565c5e149a5e7e41878c003805911
CorneeSean <radkuc@wp.pl>
2019-10-08 22:15:58 +0200

    feat(GTM): Add GtmContextDirective

---
+++ frontend/src/app/modules/shared/google-tag-manager/gtm-context.directive.ts
@@ -0,0 +1,36 @@
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
+
+interface GtmData {
+  [key: string]: string;
+}
+
+@Directive({
+  selector: '[withGtmContext]'
+})
+export class GtmContextDirective implements OnInit {
+  @Input('withGtmContext') context: string;
+  @Input() gtmData: GtmData;
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    const element = this.el.nativeElement;
+    addGtmContextToDataset(this.context, element);
+    addGtmDataToDataset(this.gtmData, element);
+  }
+}
+
+const addGtmContextToDataset = (context: string, element: HTMLElement) => {
+  if (!context) return;
+  element.dataset.gtmContext = context;
+};
+
+const addGtmDataToDataset = (gtmData: GtmData, element: HTMLElement) => {
+  if (!gtmData) return;
+  for (const [attributeName, attributeValue] of Object.entries(gtmData)) {
+    element.dataset[addGtmPrefix(attributeName)] = attributeValue;
+  }
+};
+
+const addGtmPrefix = (unprefixedName: string): string =>
+    'gtm' + unprefixedName[0].toUpperCase() + unprefixedName.slice(1);
