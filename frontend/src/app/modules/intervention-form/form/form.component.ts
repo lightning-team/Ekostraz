127a67e6d253b86f107aefd47cdc217f3071f765
CorneeSean <radkuc@wp.pl>
2019-11-23 21:23:40 +0100

    refactor: Form component clean-up

--- frontend/src/app/modules/intervention-form/form/form.component.ts
+++ frontend/src/app/modules/intervention-form/form/form.component.ts
@@ -1,27 +1,16 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { InterventionStatus } from '@shared/domain/intervention.status';
 import { GTM_CONTEXTS } from '@shared/google-tag-manager/gtm-contexts';
 import { InterventionFormData } from '../types';
+import { InterventionStatus } from '@shared/domain/intervention.status';
+import { Option } from '../helpers/option';
 
-const interventionStatuses = [
-  {
-    value: InterventionStatus.ToVerify,
-    viewValue: 'Do weryfikacji',
-  },
-  {
-    value: InterventionStatus.ActionRequired,
-    viewValue: 'Do podjęcia',
-  },
-  {
-    value: InterventionStatus.InProgress,
-    viewValue: 'W toku',
-  },
-  {
-    value: InterventionStatus.Closed,
-    viewValue: 'Zamknięta',
-  },
+export const InterventionStatusOptions = [
+  Option.of(InterventionStatus.ToVerify, 'Do weryfikacji'),
+  Option.of(InterventionStatus.ActionRequired, 'Do podjęcia'),
+  Option.of(InterventionStatus.InProgress, 'W toku'),
+  Option.of(InterventionStatus.Closed, 'Zamknięta'),
 ];
 
 /**
@@ -35,16 +24,16 @@ const interventionStatuses = [
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InterventionFormComponent implements OnInit {
-  /** Flag to decide whether the form should be in extended private mode for Ekostraż workers only. */
-  @Input() inPrivateMode = false;
   @Input() formTitle = 'Zgłoś interwencję';
   @Input() buttonText = 'Wyślij zgłoszenie';
-  /** Intervention data to fill the form with. */
   @Input() intervention: InterventionFormData | null = null;
+  @Input() inPrivateMode = false;
+
   @Output() formSubmit = new EventEmitter<InterventionFormData>();
-  interventionStatuses = interventionStatuses;
-  interventionForm = this.buildForm();
-  interventionFormGtmContext: string;
+
+  statusOptions = InterventionStatusOptions;
+  form = this.buildForm();
+  formGtmContext: string;
 
   constructor(private formBuilder: FormBuilder, @Inject(GTM_CONTEXTS) private gtmContexts) {}
 
@@ -55,45 +44,47 @@ export class InterventionFormComponent implements OnInit {
 
   private maybePatchFormValue() {
     if (this.intervention) {
-      this.interventionForm.patchValue(this.intervention);
+      this.form.patchValue(this.intervention);
     }
   }
 
   private initGtmContext() {
-    this.interventionFormGtmContext = this.inPrivateMode
+    this.formGtmContext = this.inPrivateMode
       ? this.gtmContexts.privateInterventionForm
       : this.gtmContexts.publicInterventionForm;
   }
 
-  private buildForm(): FormGroup {
-    const statusValidators = this.inPrivateMode ? [Validators.required] : [];
+  onSubmit(formValue: InterventionFormData) {
+    if (this.isFormValid()) {
+      this.formSubmit.emit(formValue);
+    }
+  }
 
-    return this.formBuilder.group({
+  isControlInvalid(controlName: string) {
+    const control = this.form.get(controlName);
+    return !control.valid && control.touched;
+  }
+
+  private buildForm(): FormGroup {
+    const formGroupConfig = {
       id: [{ value: '0', hidden: true }],
       date: [{ value: '', disabled: true }],
       name: [''],
       description: ['', Validators.required],
       phone: ['', Validators.required],
       email: ['', Validators.email],
-      status: ['', ...statusValidators],
+      status: ['', this.inPrivateMode ? [Validators.required] : []],
       address: this.formBuilder.group({
         street: ['', Validators.required],
         number: ['', Validators.required],
         city: ['', Validators.required],
       }),
-    });
-  }
+    };
 
-  onSubmit(formValue: InterventionFormData) {
-    if (!this.isFormValid()) return;
-    this.formSubmit.emit(formValue);
-  }
-
-  isControlInvalid(controlName: string) {
-    return !this.interventionForm.get(controlName).valid && this.interventionForm.get(controlName).touched;
+    return this.formBuilder.group(formGroupConfig);
   }
 
   private isFormValid(): boolean {
-    return this.interventionForm.valid;
+    return this.form.valid;
   }
 }
