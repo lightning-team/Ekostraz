ee299877a4f94edf33620015ba1297b1829b1dd7
CorneeSean <radkuc@wp.pl>
2019-12-26 16:46:15 +0100

    test(InterventionFormService): Fix unit tests

--- frontend/src/app/modules/intervention-form/service/intervention-form.service.spec.ts
+++ frontend/src/app/modules/intervention-form/service/intervention-form.service.spec.ts
@@ -1,61 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material';
 
 import { InterventionFormService } from './intervention-form.service';
 import { InterventionStatus } from '@shared/domain/intervention.status';
 import { InterventionFormData, InterventionPostData } from '@interventionForm/types';
+import { environment } from '@environment';
 
-const POST_FORM_URL = 'https://devkodawanie.azurewebsites.net/api/AddPublicForm';
-
-const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
-const snackBarMock = {
-  open: jasmine.createSpy('open'),
-};
-const SNACK_BAR_META = {
-  action: 'Zamknij',
-  config: {
-    duration: 5000,
-    verticalPosition: 'top',
-    horizontalPosition: 'center',
-  },
+const testFormData: InterventionFormData = {
+  id: '0',
+  date: null,
+  name: 'Test Name',
+  description: 'Test description',
+  phone: '12356789',
+  email: 'test@ekostraz.pl',
+  status: InterventionStatus.ToVerify,
+  address: { number: '15', city: 'New York', street: 'Wall street' },
 };
 
-const expectSnackBarOpened = (message: string) =>
-  expect(snackBarMock.open).toHaveBeenCalledWith(message, SNACK_BAR_META.action, SNACK_BAR_META.config);
-
-const createTestFormData = () =>
-  ({
-    id: '0',
-    date: null,
-    name: 'Test Name',
-    description: 'Test description',
-    phone: '12356789',
-    email: 'test@ekostraz.pl',
-    status: InterventionStatus.ToVerify,
-    address: { number: '15', city: 'New York', street: 'Wall street' },
-  } as InterventionFormData);
+const expectedRequestData = new InterventionPostData(testFormData);
+const expectedPostUrl = environment.APIUrl + 'interventions';
 
 describe('PublicFormService', () => {
   let service: InterventionFormService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let requestMock: TestRequest;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [
-        InterventionFormService,
-        { provide: Router, useValue: routerSpy },
-        { provide: MatSnackBar, useValue: snackBarMock },
-      ],
+      providers: [InterventionFormService],
     });
 
     service = TestBed.get(InterventionFormService);
-  });
-
-  afterEach(() => {
-    service = null;
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
@@ -63,50 +43,16 @@ describe('PublicFormService', () => {
   });
 
   describe('postPublicForm', () => {
-    let httpClient: HttpClient;
-    let httpTestingController: HttpTestingController;
-    let requestMock: TestRequest;
-
-    beforeEach(() => {
-      httpClient = TestBed.get(HttpClient);
-      httpTestingController = TestBed.get(HttpTestingController);
-
-      spyOn(Date.prototype, 'toISOString').and.returnValue('2019-07-30T11:34:07.791Z');
-
-      service.post(createTestFormData(), '');
-      requestMock = httpTestingController.expectOne(POST_FORM_URL);
-    });
-
-    afterEach(() => {
-      httpClient = null;
-      httpTestingController = null;
-      requestMock = null;
+    beforeEach(async () => {
+      await service.post(testFormData).subscribe();
+      requestMock = httpTestingController.expectOne(expectedPostUrl);
     });
 
     it('should send correct data to the backend', () => {
-      const expectedRequestData = new InterventionPostData(createTestFormData());
-
       expect(requestMock.request.method).toEqual('POST');
+      expect(requestMock.request.headers.get('x-functions-key')).toEqual(jasmine.any(String));
       expect(requestMock.request.withCredentials).toEqual(false);
       expect(requestMock.request.body).toEqual(expectedRequestData);
-    });
-
-    it('should open snack bar on success and navigate to home page', () => {
-      const expectedMessage = 'Dziękujemy! Twoje zgłoszenie zostało przyjęte!';
-
-      requestMock.flush({});
-
-      expectSnackBarOpened(expectedMessage);
-      expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('');
-    });
-
-    it('should open snack bar on error', () => {
-      const expectedMessage = 'Niestety, nie udało się przyjąć Twojego zgłoszenia!';
-      const errorEvent = new ErrorEvent('some error');
-
-      requestMock.error(errorEvent);
-
-      expectSnackBarOpened(expectedMessage);
     });
   });
 });
