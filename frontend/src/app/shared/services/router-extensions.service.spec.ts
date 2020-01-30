70a1bf9bd403d9618b09e7921aff605fa3775802
CorneeSean <radkuc@wp.pl>
2020-01-30 20:24:27 +0100

    feat(shared/interceptors): Add AzureLocalToken interceptor with secure local.secrets.json fetching mechanics

--- frontend/src/app/shared/services/router-extensions.service.spec.ts
+++ frontend/src/app/shared/services/router-extensions.service.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { RouterExtensionsService } from './router-extensions.service';
 
 describe('RouterExtensionsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [RouterExtensionsService],
+    }),
+  );
 
-  xit('should be created', () => {
+  it('should be created', () => {
     const service: RouterExtensionsService = TestBed.get(RouterExtensionsService);
     expect(service).toBeTruthy();
   });
