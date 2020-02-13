cea62d834ffdea376084a24190c320bb9878eb66
CorneeSean <radkuc@wp.pl>
2020-02-13 23:30:17 +0100

    fix(interventions/map): Fix map loader bug for route state interventions display

--- frontend/src/app/modules/interventions/map/map.component.ts
+++ frontend/src/app/modules/interventions/map/map.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 
-import { Subject } from 'rxjs';
-import { first, switchMapTo, tap } from 'rxjs/operators';
+import { forkJoin, Subject } from 'rxjs';
+import { first, tap } from 'rxjs/operators';
 
 import { Intervention } from '../types';
 import { InterventionsService } from '../interventions.service';
@@ -16,12 +16,9 @@ export class MapComponent {
   tilesLoadedSubject = new Subject();
   interventions: Intervention[];
   interventionMapGtmContext: string;
-
-  loading$ = this.interventionsService.getInterventions().pipe(
-    tap(data => {
-      this.interventions = data;
-    }),
-    switchMapTo(this.tilesLoadedSubject.asObservable().pipe(first())),
+  loading$ = forkJoin(
+    this.tilesLoadedSubject.asObservable().pipe(first()),
+    this.interventionsService.getInterventions().pipe(tap(data => (this.interventions = data))),
   );
 
   constructor(private interventionsService: InterventionsService, @Inject(GTM_CONTEXTS) gtmContexts) {
