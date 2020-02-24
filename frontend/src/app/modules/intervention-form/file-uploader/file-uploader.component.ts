641c08b4f0befabcd5b5ecc0887cf7d4767fc476
CorneeSean <radkuc@wp.pl>
2020-02-25 00:50:22 +0100

    feat(frontend/intervention-form): Add FileUploaderComponent

---
+++ frontend/src/app/modules/intervention-form/file-uploader/file-uploader.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+export enum SupportedFileTypes {
+  Images = 'image/*',
+  Pdf = 'application/pdf',
+  WordDocument = 'application/msword',
+  Text = 'text/plain',
+  Video = 'video/*',
+  Audio = 'audio/*',
+}
+
+@Component({
+  selector: 'eko-file-uploader',
+  templateUrl: './file-uploader.component.html',
+  styleUrls: ['./file-uploader.component.scss'],
+})
+export class FileUploaderComponent implements OnInit {
+  @Input() imagesOnly = false;
+  @Input() editTemporaryDisabled = false;
+
+  files: Array<File> = [];
+  addFilesMessage: string;
+  acceptedFileTypes: string;
+
+  ngOnInit(): void {
+    this.addFilesMessage = `Dodaj ${this.imagesOnly ? 'zdjęcia' : 'pliki'}`;
+    this.acceptedFileTypes = this.imagesOnly ? SupportedFileTypes.Images : this.getAllSupportedTypes();
+  }
+
+  openFileBrowser(input: HTMLInputElement) {
+    input.click();
+  }
+
+  onFilesSelected(input: HTMLInputElement) {
+    const newFiles = this.filterExistingFiles(input.files, this.files);
+    this.files = this.files.concat(newFiles);
+  }
+
+  private filterExistingFiles(inputFiles: FileList, existingFiles: File[]) {
+    return Array.from(inputFiles).filter(
+      addedFile =>
+        !existingFiles.find(
+          existingFile =>
+            existingFile.name === addedFile.name &&
+            existingFile.type === addedFile.type &&
+            existingFile.size === addedFile.size,
+        ),
+    );
+  }
+
+  onFileRemove(removedFile: File) {
+    this.files = this.files.filter(file => file !== removedFile);
+  }
+
+  private getAllSupportedTypes(): string {
+    return Object.values(SupportedFileTypes).join(',');
+  }
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -6,10 +6,23 @@ import { LoaderComponent } from './components/loader/loader.component';
 import { GtmScriptComponent } from './google-tag-manager/gtm-script.component';
 import { GtmContextDirective } from './google-tag-manager/gtm-context.directive';
 import { InterventionStatusPipe } from './pipes/intervention-status.pipe';
+import { FileUploaderComponent } from '@interventionForm/file-uploader/file-uploader.component';
+import { FileThumbComponent } from '@interventionForm/file-uploader/file-thumb/file-thumb.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+const MATERIAL_IMPORTS = [MatIconModule, MatButtonModule];
 
 @NgModule({
-  declarations: [GtmScriptComponent, GtmContextDirective, InterventionStatusPipe, LoaderComponent],
-  imports: [MatProgressSpinnerModule, CommonModule],
-  exports: [GtmScriptComponent, GtmContextDirective, InterventionStatusPipe, LoaderComponent],
+  declarations: [
+    GtmScriptComponent,
+    GtmContextDirective,
+    InterventionStatusPipe,
+    LoaderComponent,
+    FileUploaderComponent,
+    FileThumbComponent,
+  ],
+  imports: [MatProgressSpinnerModule, CommonModule, ...MATERIAL_IMPORTS],
+  exports: [GtmScriptComponent, GtmContextDirective, InterventionStatusPipe, LoaderComponent, FileUploaderComponent],
 })
 export class SharedModule {}
