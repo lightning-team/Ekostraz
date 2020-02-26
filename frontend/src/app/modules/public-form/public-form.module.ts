a0e5f424c39fa9d9e7434bd9f66068c76f273667
CorneeSean <radkuc@wp.pl>
2020-02-26 21:37:41 +0100

    feat(frontend): Use FileUploaderComponent in intervention forms

--- frontend/src/app/modules/public-form/public-form.module.ts
+++ frontend/src/app/modules/public-form/public-form.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSnackBarModule } from '@angular/material';
 
+import { SharedModule } from '@shared/shared.module';
 import { InterventionFormModule } from '@interventionForm/intervention-form.module';
 import { PublicFormComponent } from './public-form/public-form.component';
 
 @NgModule({
   declarations: [PublicFormComponent],
-  imports: [CommonModule, InterventionFormModule, MatSnackBarModule],
+  imports: [CommonModule, InterventionFormModule, MatSnackBarModule, SharedModule],
   exports: [PublicFormComponent],
 })
 export class PublicFormModule {}
