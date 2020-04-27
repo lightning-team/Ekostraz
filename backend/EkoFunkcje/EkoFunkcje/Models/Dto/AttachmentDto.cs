96e265a9cf6de77ae8b5f2a6d6f159e6dd0c4dc2
CorneeSean <radkuc@wp.pl>
2020-04-27 21:01:54 +0200

    fix(backend/attachments): Use ContentDisposition header for extended attachment data handling

---
+++ backend/EkoFunkcje/EkoFunkcje/Models/Dto/AttachmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EkoFunkcje.Models.Dto
+{
+    public class AttachmentDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public string Type { get; set; }
+    }
+}
