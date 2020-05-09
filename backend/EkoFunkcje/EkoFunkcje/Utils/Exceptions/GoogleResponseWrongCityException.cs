cb60bf157d0095c44060d93f2db291fecd9b730e
lukasz.przerada <lukasz.przerada@ceneo.pl>
2020-05-09 15:38:40 +0200

    Add response address validation

---
+++ backend/EkoFunkcje/EkoFunkcje/Utils/Exceptions/GoogleResponseWrongCityException.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace EkoFunkcje.Utils.Exceptions
+{
+    public class GoogleResponseWrongCityException : BaseException
+    {
+        public GoogleResponseWrongCityException(string sentCity, string responseCity) : base($"Podane miasto ({sentCity}) nie zgadza się ze zwróconym miastem: {responseCity}", LogLevel.Error)
+        {
+        }
+    }
+}
