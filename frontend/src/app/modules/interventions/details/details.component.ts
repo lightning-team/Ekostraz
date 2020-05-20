50c42f28a17663222bb9dd22b7780050c0a6d1b1
CorneeSean <radkuc@wp.pl>
2020-05-21 01:58:33 +0200

    feat(frontend/intervention-details): Style intervention details view with use of accordion

--- frontend/src/app/modules/interventions/details/details.component.ts
+++ frontend/src/app/modules/interventions/details/details.component.ts
@@ -1,25 +1,17 @@
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Intervention } from '@shared/domain/intervention.model';
-import { ComponentWithSubscriptions } from '@shared/components/base';
-import { GTM_CONTEXTS } from '@shared/google-tag-manager/gtm-contexts';
 
 @Component({
   selector: 'eko-intervention-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent extends ComponentWithSubscriptions {
+export class DetailsComponent {
   @Input() intervention: Intervention;
-  @Input() embedded?: boolean;
 
-  interventionDetailsGtmContext: string;
-
-  constructor(private router: Router, @Inject(GTM_CONTEXTS) gtmContexts) {
-    super();
-    this.interventionDetailsGtmContext = gtmContexts.interventionDetails;
-  }
+  constructor(private router: Router) {}
 
   navigateToEditView() {
     this.router.navigate(['interwencje', this.intervention.id, 'edytuj']);
