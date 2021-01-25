19895168a5dd8e71533429609274c8017b9cae90
CorneeSean <radkuc@wp.pl>
2021-01-25 20:58:11 +0100

    test(frontend/interventions): Fix test runs for Interventions Filters component

--- frontend/src/app/modules/interventions/list/filters/filters.component.spec.ts
+++ frontend/src/app/modules/interventions/list/filters/filters.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule, MatFormFieldModule, MatSelectModule } from '@angular/material';
 
 import { FiltersComponent } from './filters.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
@@ -8,6 +11,7 @@ describe('FiltersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatSelectModule, NoopAnimationsModule],
       declarations: [FiltersComponent],
     }).compileComponents();
   }));
@@ -15,6 +19,7 @@ describe('FiltersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FiltersComponent);
     component = fixture.componentInstance;
+    component.filtersState = {};
     fixture.detectChanges();
   });
 
