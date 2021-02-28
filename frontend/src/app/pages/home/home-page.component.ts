bd931d750915f93a3afe1dc0b8b96df2ea81e5af
CorneeSean <radkuc@wp.pl>
2021-02-28 16:07:15 +0100

    refactor(frontend/pages/home): Replace button with anchor tag and constant route.

--- frontend/src/app/pages/home/home-page.component.ts
+++ frontend/src/app/pages/home/home-page.component.ts
@@ -1,13 +1,15 @@
 import { Component, Inject } from '@angular/core';
 import { GTM_CONTEXTS } from '@shared/google-tag-manager/gtm-contexts';
+import { EkoRoutePaths } from '@app/eko-route-paths';
 
 @Component({
-  selector: 'app-home-page',
+  selector: 'eko-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
   homePageGtmContext: string;
+  publicFormRoute = EkoRoutePaths.PublicForm;
 
   constructor(@Inject(GTM_CONTEXTS) gtmContexts) {
     this.homePageGtmContext = gtmContexts.homePage;
