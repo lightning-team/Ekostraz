75653ec0651047df387f56e4126e25ecd3d959af
Dariusz Parzygnat <dariusz.parzygnat@gmail.com>
2021-06-04 17:15:42 +0200

    version v3

--- backend/EkoFunkcje/EkoFunkcje/Startup.cs
+++ backend/EkoFunkcje/EkoFunkcje/Startup.cs
@@ -1,13 +1,10 @@
-﻿using System.Reflection;
-using AzureFunctions.Extensions.Swashbuckle;
+﻿using AzureFunctions.Extensions.Swashbuckle;
 using EkoFunkcje.Utils;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters.Json.Internal;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+using System.Reflection;
 
 [assembly: FunctionsStartup(typeof(EkoFunkcje.Startup))]
 
@@ -17,7 +14,7 @@ namespace EkoFunkcje
     {
         public void Configure(IWebJobsBuilder builder)
         {
-            builder.Services.AddTransient<IConfigureOptions<MvcOptions>, MvcJsonMvcOptionsSetup>(); //https://github.com/Azure/azure-webjobs-sdk-extensions/issues/486
+            //builder.Services.AddTransient<IConfigureOptions<MvcOptions>, MvcJsonMvcOptionsSetup>(); //https://github.com/Azure/azure-webjobs-sdk-extensions/issues/486
             builder.Services.AddHttpClient();
             builder.Services.AddSingleton<IAddressConverter, AddressConverter>();
             builder.Services.AddSingleton<IAddressValidator, AddressValidator>();
